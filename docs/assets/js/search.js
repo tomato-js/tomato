169264741f89c1b5427d6de8a4577350b8ff99e3
刘放 <362512489@qq.com>
2020-02-24 19:53:27 +0800

    docs: add docs

---
+++ docs/assets/js/search.js
@@ -0,0 +1,75 @@
+var typedoc = typedoc || {};
+typedoc.search = typedoc.search || {};
+typedoc.search.data = {
+  kinds: { "1": "External module", "64": "Function" },
+  rows: [
+    {
+      id: 0,
+      kind: 1,
+      name: '"element/src/get"',
+      url: "modules/_element_src_get_.html",
+      classes: "tsd-kind-external-module"
+    },
+    {
+      id: 1,
+      kind: 64,
+      name: "get",
+      url: "modules/_element_src_get_.html#get",
+      classes: "tsd-kind-function tsd-parent-kind-external-module",
+      parent: '"element/src/get"'
+    },
+    {
+      id: 2,
+      kind: 1,
+      name: '"env/src/judge"',
+      url: "modules/_env_src_judge_.html",
+      classes: "tsd-kind-external-module"
+    },
+    {
+      id: 3,
+      kind: 64,
+      name: "isExist",
+      url: "modules/_env_src_judge_.html#isexist",
+      classes: "tsd-kind-function tsd-parent-kind-external-module",
+      parent: '"env/src/judge"'
+    },
+    {
+      id: 4,
+      kind: 64,
+      name: "isURLSearchParamsExist",
+      url: "modules/_env_src_judge_.html#isurlsearchparamsexist",
+      classes: "tsd-kind-function tsd-parent-kind-external-module",
+      parent: '"env/src/judge"'
+    },
+    {
+      id: 5,
+      kind: 1,
+      name: '"path/src/parse"',
+      url: "modules/_path_src_parse_.html",
+      classes: "tsd-kind-external-module"
+    },
+    {
+      id: 6,
+      kind: 64,
+      name: "parse",
+      url: "modules/_path_src_parse_.html#parse",
+      classes: "tsd-kind-function tsd-parent-kind-external-module",
+      parent: '"path/src/parse"'
+    },
+    {
+      id: 7,
+      kind: 1,
+      name: '"shared/src/is-empty"',
+      url: "modules/_shared_src_is_empty_.html",
+      classes: "tsd-kind-external-module"
+    },
+    {
+      id: 8,
+      kind: 64,
+      name: "isEmptyObject",
+      url: "modules/_shared_src_is_empty_.html#isemptyobject",
+      classes: "tsd-kind-function tsd-parent-kind-external-module",
+      parent: '"shared/src/is-empty"'
+    }
+  ]
+};
