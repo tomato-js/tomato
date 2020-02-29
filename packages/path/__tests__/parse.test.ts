06e4da83b7203f09142d8eed8fb2677186af643e
刘放 <362512489@qq.com>
2020-02-29 20:07:30 +0800

    feat(path): add decode logic for parse

--- packages/path/__tests__/parse.test.ts
+++ packages/path/__tests__/parse.test.ts
@@ -7,11 +7,34 @@ describe("path util", () => {
       const parsed = path.parse("");
       expect(parsed).toBe(null);
     });
-    test("parse(https://www.baidu.com?a=123&b=456) with queryString", () => {
+    test("parse(?a=123&b=456) with queryString", () => {
       const parsed = path.parse("?a=123&b=456");
       expect(parsed).toStrictEqual({
         a: "123",
         b: "456"
+      });
+    });
+    test("parse() some need encode", () => {
+      const parsed = path.parse(
+        "?queryKey=this%20is%20queryA&url=https%3A%2F%2Ftomato-js.github.io%2Ftomato%2Findex.html&%E4%B8%AD%E6%96%87=%E4%BD%A0%E5%A5%BD"
+      );
+      expect(parsed).toStrictEqual({
+        queryKey: "this+is+queryA",
+        url: "https://tomato-js.github.io/tomato/index.html",
+        中文: "你好"
+      });
+    });
+    test("parse() some need encode but whit options.encode=false", () => {
+      const parsed = path.parse(
+        "?queryKey=this%20is%20queryA&url=https%3A%2F%2Ftomato-js.github.io%2Ftomato%2Findex.html&%E4%B8%AD%E6%96%87=%E4%BD%A0%E5%A5%BD",
+        {
+          encode: false
+        }
+      );
+      expect(parsed).toStrictEqual({
+        queryKey: "this+is+queryA",
+        "%E4%B8%AD%E6%96%87": "%E4%BD%A0%E5%A5%BD",
+        url: "https%3A%2F%2Ftomato-js.github.io%2Ftomato%2Findex.html"
       });
     });
     test("parse(https://www.baidu.com?a=123&b=456) with queryString, but URLSearchParams in not exists", () => {
