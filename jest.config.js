e51a63f3e834d347cefee490558803a8810c94ab
刘放 <362512489@qq.com>
2020-02-29 20:58:53 +0800

    test: add dist into jest coveragePathIgnore

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,6 @@ module.exports = {
   // Replace `ts-jest` with the preset you want to use
   // from the above list
   preset: "ts-jest",
-  testMatch: ["**/packages/**/*.test.[jt]s?(x)"]
+  testMatch: ["**/packages/**/*.test.[jt]s?(x)"],
+  coveragePathIgnorePatterns: ["/node_modules/", "/dist/"]
 };
