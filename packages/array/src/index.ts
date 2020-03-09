e0f42658da4832654085c6221fcd53eab2a8e2e1
刘放 <362512489@qq.com>
2020-03-09 23:25:25 +0800

    feat(array): sample and sample-size

--- packages/array/src/index.ts
+++ packages/array/src/index.ts
@@ -2,3 +2,5 @@ export { allEqual } from "./all-equal";
 export { countOccurrences } from "./count-occurrences";
 export { deepFlatten } from "./deep-flatten";
 export { filterNonUnique } from "./filter-non-unique";
+export { sampleSize } from "./sample-size";
+export { sample } from "./sample";
---
+++ packages/array/src/sample.ts
@@ -0,0 +1,22 @@
+import { sampleSize } from "./sample-size";
+
+/**
+ * @packageDocumentation
+ * @module @tomato-js/array
+ */
+
+/**
+ * 在数组中随机取1个值
+ *
+ * 脚本举例
+ * ```
+ *   import { sample } from '@tomato-js/array'
+ *   sample([3, 7, 9, 11]); // 9
+ * ```
+ *
+ * @param arr - 原始数组
+ * @returns 随机值
+ */
+export function sample([...arr]) {
+  return sampleSize(arr, 1)[0];
+}
