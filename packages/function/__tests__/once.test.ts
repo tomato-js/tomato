904ad24722e43cf23bc7182b66861ff8f2817590
刘放 <362512489@qq.com>
2020-03-19 13:10:30 +0800

    test: for once

--- packages/function/__tests__/once.test.ts
+++ packages/function/__tests__/once.test.ts
@@ -11,5 +11,14 @@ describe("function util", () => {
       addOnce();
       expect(num).toBe(2);
     });
+    test("once() without type", () => {
+      let num = "2";
+      const add = () => (num = num + 1);
+      const addOnce = func.once(add);
+      addOnce();
+      addOnce();
+      addOnce();
+      expect(num).toBe("21");
+    });
   });
 });
